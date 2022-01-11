e611356242f8a3e43f17d7bec41e56c433d4c60b
Pazyljon Tursun <pazyljon.tursun@SGD-C02GJ0ARML87.fios-router.home>
2022-01-11 11:26:42 -0500

    add output file

---
+++ -outputs.tf
@@ -0,0 +1,5 @@
+# Keeping this as optional for now 
+ /* output "conformance_pack_arn" {
+   description = "ARN of aws config conformance pack"
+   value = aws_config_conformance_pack.this.arn
+} */
