00070a52b1a2c1cb1b836678c5952bb819119dce
Pazyljon Tursun <pazyljon.tursun@SGD-C02GJ0ARML87.fios-router.home>
2022-01-11 11:50:11 -0500

    remove files in main branch

--- -outputs.tf
+++
@@ -1,5 +0,0 @@
-# Keeping this as optional for now 
- /* output "conformance_pack_arn" {
-   description = "ARN of aws config conformance pack"
-   value = aws_config_conformance_pack.this.arn
-} */
--- versions.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_providers {
-    aws = {
-      version = ">= 3.63"
-    }
-  }
-}
